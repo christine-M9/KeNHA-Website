0694d7b2484bcac725cb4a78d8f6b604fdf7b68b
Christine <christinekhekama99@gmail.com>
2023-10-02 15:07:21 +0300

    fetching data from flask to react frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [projects, setProjects] = useState([]);
+  const [services, setServices] = useState([]);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    // Fetch projects
+    fetch('api/projects')
+      .then(response => response.json())
+      .then(data => setProjects(data))
+      .catch(error => console.error('Error fetching projects:', error));
+
+
+    // Fetch services
+    fetch('api/services')
+      .then(response => response.json())
+      .then(data => setServices(data))
+      .catch(error => console.error('Error fetching services:', error));
+
+    // Fetch contacts
+    fetch('api/contacts')
+      .then(response => response.json())
+      .then(data => setContacts(data))
+      .catch(error => console.error('Error fetching contacts:', error));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>WELCOME TO KENYA NATIONAL HIGHWAYS AUTHORITY</h1>
+
+      <h2>Projects</h2>
+      <ul>
+        {projects.map(project => (
+          <li key={project.id}>
+            Title: {project.title}, Description: {project.description}
+          </li>
+        ))}
+      </ul>
+
+      <h2>Services</h2>
+      <ul>
+        {services.map(service => (
+          <li key={service.id}>
+            Title: {service.title}, Description: {service.description}
+          </li>
+        ))}
+      </ul>
+
+      <h2>Contacts</h2>
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact.id}>
+            Name: {contact.name}, Email: {contact.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
