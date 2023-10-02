1815374d3acc80d3a6f6d6c6cd8e149dca338e3f
Christine <christinekhekama99@gmail.com>
2023-10-02 14:36:08 +0300

    seeding

---
+++ backend/seed.py
@@ -0,0 +1,38 @@
+from faker import Faker
+from app import app, db, Project, Service, Contact
+
+fake = Faker()
+
+# Set up app context
+with app.app_context():
+
+    # Seed projects
+    for _ in range(5):
+        project = Project(
+            title=fake.sentence(),
+            description=fake.paragraph()
+        )
+        db.session.add(project)
+
+        # Seed services and associate them with the project
+        for _ in range(fake.random_int(min=1, max=5)):  # Adjust max as needed
+            service = Service(
+                title=fake.sentence(),
+                description=fake.paragraph()
+            )
+            project.services.append(service)
+
+    # Seed contacts
+    for _ in range(5):
+        contact = Contact(
+            name=fake.name(),
+            email=fake.email(),
+            message=fake.paragraph(),
+            project_id=fake.random_int(min=1, max=5)  # Assuming you have 5 projects
+        )
+        db.session.add(contact)
+
+    # Commit the changes
+    db.session.commit()
+
+
